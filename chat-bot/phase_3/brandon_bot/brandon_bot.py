b52d8514985b42e2c845651722ca9855b7e31c50
bkwe <bkwe@ucsc.edu>
2023-11-27 18:56:30 -0800

    brandon-bot

--- chat-bot/phase_3/brandon_bot/brandon_bot.py
+++ chat-bot/phase_3/brandon_bot/brandon_bot.py
@@ -1,11 +1,32 @@
 import wikipediaapi
 import re
 
+wiki_wiki = wikipediaapi.Wikipedia('MyProjectName (merlin@example.com)', 'en')
+
 def brandon_bot(irc, msg, sender, channel):
     if re.search('when was [a-zA-Z ]+ born\?', msg):
         name = ' '.join(msg.split()[2:-1])
-        print(f"NAME: {name}")
-        #TODO
-    
-    print("Called brandon_bot()")
-    return
+        
+        page_py = wiki_wiki.page(name)
+        summary = page_py.summary
+
+        start = summary.find('(')
+        end = summary.find(')')
+        birthday = summary[start+1:end]
+        idx = birthday.find("born")
+        if idx >= 0:
+            irc.send(channel, birthday[idx+5:])
+            return
+        
+        idx = birthday.find(" – ")
+        if idx >=0:
+            n = idx
+            while n > 0 and birthday[n] != ';':
+                n -= 1
+            if n > 0:
+                n += 2
+            irc.send(channel, birthday[n:idx])
+            return
+            
+        irc.send(channel, f"I'm sorry, I'm not sure when {name} was born.")
+        return
